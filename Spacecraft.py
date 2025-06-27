4b205e2493f57084a29541aab4689b76beb6486c
atommnava <atommunoznava@gmail.com>
2025-06-27 15:56:40 -0600

    Drawing the tangent line

--- Spacecraft.py
+++ Spacecraft.py
@@ -1,5 +1,4 @@
 from main import window, RED, OBJ_SIZE
-
 import pygame.draw
 
 
@@ -13,3 +12,7 @@ class Spacecraft:
 
     def draw(self):
         pygame.draw.circle(window, RED, (int(self.x), int(self.y)), OBJ_SIZE)
+
+    def move(self, planet=None):
+        self.x += self.vel_x
+        self.y += self.vel_y
